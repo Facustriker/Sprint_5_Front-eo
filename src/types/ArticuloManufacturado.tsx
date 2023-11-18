41e259f5198cbbcb2f117bd99b8e5749e2feb262
Fabrizio Avallone <143529524+Fabrixx19@users.noreply.github.com>
2023-11-18 10:44:59 -0300

    Delete src directory

--- src/types/ArticuloManufacturado.tsx
+++
@@ -1,9 +0,0 @@
-export interface ArticuloManufacturado{
-    id:number;
-    denominacion: string;
-    descripcion:string;
-    tiempoEstimadoCocina: number;
-    precioVenta: number;
-    urlImagen:string;
-    
-}
